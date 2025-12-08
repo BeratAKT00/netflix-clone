a705804e32b27d956f9f5c62195d483752eefad8
BeratAKT00 <berataktasdev@gmail.com>
2025-12-08 17:11:13 +0100

    Création page Ma Liste et finalisation Watchlist

--- src/Controller/WatchlistController.php
+++ src/Controller/WatchlistController.php
@@ -11,24 +11,33 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class WatchlistController extends AbstractController
 {
+    // 1. La nouvelle page pour VOIR la liste
+    #[Route('/my-list', name: 'app_watchlist_index')]
+    #[IsGranted('ROLE_USER')]
+    public function index(): Response
+    {
+        return $this->render('watchlist/index.html.twig', [
+            // On récupère la liste de l'utilisateur connecté
+            'movies' => $this->getUser()->getWatchlist(),
+        ]);
+    }
+
+    // 2. L'action pour AJOUTER/RETIRER (qu'on a fait avant)
     #[Route('/watchlist/add/{id}', name: 'app_watchlist_toggle')]
-    #[IsGranted('ROLE_USER')] // Sécurité : Il faut être connecté
+    #[IsGranted('ROLE_USER')]
     public function toggle(Movie $movie, EntityManagerInterface $entityManager): Response
     {
         /** @var \App\Entity\User $user */
         $user = $this->getUser();
 
-        // Si le film est déjà dans la liste, on l'enlève
         if ($user->getWatchlist()->contains($movie)) {
             $user->removeWatchlist($movie);
         } else {
-            // Sinon, on l'ajoute
             $user->addWatchlist($movie);
         }
 
         $entityManager->flush();
 
-        // On revient sur la page du film
         return $this->redirectToRoute('app_movie_show', ['id' => $movie->getId()]);
     }
 }
